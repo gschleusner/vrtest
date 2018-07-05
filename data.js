1f4485e33c12197e3b329a2fc5a88fcf0a6341e0
gschleusner <greg.schleusner@Hok.com>
2018-07-05 13:23:34 -0400

    Frist
    First

---
+++ data.js
@@ -0,0 +1,155 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-seatac_equi_2_72",
+      "name": "SeaTAc_Equi_2_72",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3200,
+      "initialViewParameters": {
+        "yaw": -3.0735318347633562,
+        "pitch": -0.006142009083296429,
+        "fov": 1.5187863039119234
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.061856352432833,
+          "pitch": -0.07087887660904713,
+          "rotation": 0,
+          "target": "1-seatac_equi_3_72"
+        },
+        {
+          "yaw": -3.0180582164587086,
+          "pitch": -0.05044678941239411,
+          "rotation": 0,
+          "target": "2-seatac_equi_1_72"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-seatac_equi_3_72",
+      "name": "SeaTAc_Equi_3_72",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3200,
+      "initialViewParameters": {
+        "yaw": 0.10407220237784642,
+        "pitch": 0.03742656499809449,
+        "fov": 1.5187863039119234
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.656593181003684,
+          "pitch": -0.007719041779598257,
+          "rotation": 0,
+          "target": "2-seatac_equi_1_72"
+        },
+        {
+          "yaw": -3.0144888432132895,
+          "pitch": 0.04451897662362647,
+          "rotation": 0,
+          "target": "0-seatac_equi_2_72"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-seatac_equi_1_72",
+      "name": "SeaTAc_Equi_1_72",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3200,
+      "initialViewParameters": {
+        "yaw": 0.6296610916810366,
+        "pitch": 0.07815976300833505,
+        "fov": 1.5187863039119234
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.131884042528041,
+          "pitch": 0.0844710459271063,
+          "rotation": 0,
+          "target": "0-seatac_equi_2_72"
+        },
+        {
+          "yaw": -0.57348195261714,
+          "pitch": 0.01659296688604428,
+          "rotation": 0,
+          "target": "2-seatac_equi_1_72"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Setac Demo",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
